95f9c70b155caf15be127712d928e9c3c379ca28
vek03 <vektromboni@gmail.com>
2025-11-26 15:14:51 -0300

    fix: graylog

--- src/main/java/com/vekrest/vekconsumerapi/vekconsumerapi/VekconsumerapiApplication.java
+++ src/main/java/com/vekrest/vekconsumerapi/vekconsumerapi/VekconsumerapiApplication.java
@@ -1,13 +1,21 @@
 package com.vekrest.vekconsumerapi.vekconsumerapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VekconsumerapiApplication {
+public class VekconsumerapiApplication implements CommandLineRunner {
+	private static final Logger LOG = LoggerFactory.getLogger(VekconsumerapiApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(VekconsumerapiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("VEKREST -> VEKCONSUMERAPI - INICIALIZADO COM SUCESSO!");
+	}
 }
